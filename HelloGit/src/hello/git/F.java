32756a0eb1ad48f1f45f38c97402676a351af7df
doha1993 <66931884+doha1993@users.noreply.github.com>
2020-08-21 13:47:37 +0900

    add commentttttttttttttttttttttttttttttttttt

--- HelloGit/src/hello/git/F.java
+++ HelloGit/src/hello/git/F.java
@@ -3,7 +3,9 @@ package hello.git;
 public class F {
 
   public void f(){
-    System.out.println("git에서 작업했습니다.");
-    
+    //github 저장소에서 작업했습니다.
+    //하하하하하ㅏ하하핳
+    //fetch
+    //meget
   }
 }
